08e39323a5d0a7c500c510a409a27cc3636b3783
Hobofan <goisser94@gmail.com>
2016-09-21 21:24:44 +0200

    little refactor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::io::BufRead;
 use std::io::Write;
 use std::fs::File;
 use std::fs::create_dir_all;
-use std::path::Path;
 
 use rustc_serialize::json::Json;
 
@@ -13,9 +12,9 @@ static INPUT_NAME: &'static str = "latest-all.json";
 static OUTPUT_DIR: &'static str = "split";
 
 fn main() {
-    let mut file = File::open(INPUT_NAME).unwrap();
-    let mut reader = BufReader::new(&file);
-    // println!("{:?}", &reader.lines().count());
+    let file = File::open(INPUT_NAME).unwrap();
+    let reader = BufReader::new(&file);
+
     for line in reader.lines() {
         let l = line.unwrap();
         let line_length = l.len();
@@ -23,32 +22,39 @@ fn main() {
         if line_length == 1 {
             continue;
         }
+
         let json_part = &l[0..(line_length - 1)];
         let data = Json::from_str(&json_part).unwrap();
         let id = data.as_object().unwrap().get("id").unwrap().as_string().unwrap();
 
-        let mut dir_name = OUTPUT_DIR.to_owned();
-        let mut skip_chars = 0;
-        if id.len() > 3 {
-            dir_name = dir_name.to_owned() + "/";
-            dir_name = dir_name.to_owned() + &id[0..3];
-            skip_chars = 3;
-        }
-        if id.len() > 6 {
-            dir_name = dir_name.to_owned() + "/";
-            dir_name = dir_name.to_owned() + &id[3..6];
-            skip_chars = 6;
-        }
-        if id.len() > 9 {
-            dir_name = dir_name.to_owned() + "/";
-            dir_name = dir_name.to_owned() + &id[6..9];
-            skip_chars = 9;
-        }
-        let file_name = dir_name.to_owned() + "/" + &id[skip_chars..] + ".json";
+        let (dir_name, file_name) = paths(OUTPUT_DIR, id);
         println!("{} - {}", id, file_name);
 
         create_dir_all(dir_name).unwrap();
         let mut out_file = File::create(file_name).unwrap();
         out_file.write_all(&json_part.as_bytes()).unwrap();
     }
+}
+
+fn paths(output_dir: &str, id: &str) -> (String, String) {
+    let mut dir_name = output_dir.to_owned();
+    let mut skip_chars = 0;
+    if id.len() > 3 {
+        dir_name = dir_name.to_owned() + "/";
+        dir_name = dir_name.to_owned() + &id[0..3];
+        skip_chars = 3;
+    }
+    if id.len() > 6 {
+        dir_name = dir_name.to_owned() + "/";
+        dir_name = dir_name.to_owned() + &id[3..6];
+        skip_chars = 6;
+    }
+    if id.len() > 9 {
+        dir_name = dir_name.to_owned() + "/";
+        dir_name = dir_name.to_owned() + &id[6..9];
+        skip_chars = 9;
+    }
+    let file_name = dir_name.to_owned() + "/" + &id[skip_chars..] + ".json";
+
+    return (dir_name, file_name);
 }
